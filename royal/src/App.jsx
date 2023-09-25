004f0d730ba077fdb228f10a1d544e48f408ce3d
Eddie Maceda <eddiemaceda@Eddies-MacBook-Air.local>
2023-09-25 18:58:18 -0400

    Add Header Component to App

--- royal/src/App.jsx
+++ royal/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 import Banner from "./components/Banner";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Banner />
+      <Header />
     </>
   );
 }
