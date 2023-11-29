6cb4563e0702e5b4277bd814e28e071b2a4ebe71
Eddie Maceda <eddiemaceda11@gmail.com>
2023-11-28 19:00:56 -0500

    Import App

--- royal/src/main.jsx
+++ royal/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import App from "./App";
+
 import "./index.css";
 
 const router = createBrowserRouter([
